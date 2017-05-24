a622e85ee5169845d2e60125b327656dfb43a7f3
Rukshan Perera <rukshan@vginthw192.internal.synopsys.com>
2017-05-24 12:48:32 +0530

    regex added

--- src/data/WorkingPersonDaoHashMap.cpp
+++ src/data/WorkingPersonDaoHashMap.cpp
@@ -5,9 +5,13 @@
 #include <array>
 #include <exception>
 #include <fstream>
+#include <regex>
 
+#include "util/easylogging++.h"
 #include "model/WorkingPersonFactory.h"
 #include "model/WorkingPerson.h"
+
+INITIALIZE_EASYLOGGINGPP
 
 namespace eatms
 {
@@ -107,14 +111,27 @@ namespace eatms
 
         void WorkingPersonDaoHashMap::loadFromFile_(std::string fileName){
             std::ifstream inFile (fileName);
-            if(!inFile.is_open()){
 
+            if(!inFile.is_open()){
+                LOG(ERROR) << "My first info log using default logger";
             }else{
                 std::string line;
+                std::cmatch matches;
+                std::regex e ("^(\\s*)(EM|TR)(\\d{0,3})\\s*,(\\s*\\w+\\s*)(,\\s*\\d*\\s*){2,3}$");
                 while(std::getline(inFile, line)){
-
+                    if(std::regex_match(line.c_str(), matches, e)){
+                        std::vector<std::string> processedList;
+                        //TODO : add to hash map after creating the string vectors
+                        for (int i = 0; i < matches.size(); ++i) {
+                            LOG(DEBUG) << "[" << matches[i] << "] ";
+                            //                            processedList.push_back();
+                        }
+                    }else{
+                        LOG(WARNING) << "Invalide line! ignored : " << line ;
+                    }
                 }
                 inFile.close();
+
             }
         }
 
@@ -133,21 +150,50 @@ namespace eatms
             }else{
                 float totalpay = 0.0f;
 
-
                 for (int i = 0; i < EATMS_MAX_EMPLOYEE_NUM; ++i) {
                     if(employees_[i]){
                         totalpay += employees_[i]->getMonthlyPay();
                     }
                 }
+
                 for (int i = 0; i < EATMS_MAX_TRAINEE_NUM; ++i) {
                     if(trainees_[i]){
                         totalpay += trainees_[i]->getMonthlyPay();
                     }
                 }
+
                 cachedTotalPay_   = totalpay;
                 cacheInvalidated_ = false;
                 return totalpay;
             }
+        }
+
+        std::forward_list<std::array<std::string,3>> WorkingPersonDaoHashMap::listMonthlyPay(){
+            std::forward_list<std::array<std::string,3>> retList;
+
+            for (int i = 0; i < EATMS_MAX_EMPLOYEE_NUM; ++i) {
+                const model::WorkingPerson * tempPerson = employees_[i];
+                if(tempPerson){
+                    std::array<std::string,3> tmpArray;
+                    tmpArray[0] = tempPerson->getId();
+                    tmpArray[1] = tempPerson->getName();
+                    tmpArray[2] = std::to_string(tempPerson->getMonthlyPay());
+                    retList.push_front(tmpArray);
+                }
+            }
+
+            for (int i = 0; i < EATMS_MAX_TRAINEE_NUM; ++i) {
+                const model::WorkingPerson * tempPerson = trainees_[i];
+                if(tempPerson){
+                    std::array<std::string,3> tmpArray;
+                    tmpArray[0] = tempPerson->getId();
+                    tmpArray[1] = tempPerson->getName();
+                    tmpArray[2] = std::to_string(tempPerson->getMonthlyPay());
+                    retList.push_front(tmpArray);
+                }
+            }
+
+            return retList;
         }
 
 
